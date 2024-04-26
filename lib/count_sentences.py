dd48b8ff49db3b56891909c6855bda8c44aba700
Brenden Hartman <brendenhart22@gmail.com>
2024-04-26 14:27:53 -0500

    made a semi complex MyString python class

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,46 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value = ""):
+        if type(value) is str:
+            self._value = value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        value_last = [letter for letter in self.value][-1]
+        if value_last == ".":
+            return True
+        else:
+            return False
+        
+    def is_question(self):
+        value_last = [letter for letter in self.value][-1]
+        if value_last == "?":
+            return True
+        else:
+            return False
+    def is_exclamation(self):
+        value_last = [letter for letter in self.value][-1]
+        if value_last == "!":
+            return True
+        else:
+            return False
+        
+    def count_sentences(self):
+        #self.value.replace("!",".").replace("?", ".").split(". ")
+        if self.value != "": 
+            return len(self.value.replace("!",".").replace("?", ".").split(". "))
+        else:
+            return 0
+        
+    def get_value(self):
+        return self._value
+
+    def set_value(self, value):
+        if type(value) is str:
+            self._value = value
+        else:
+            print("The value must be a string.")  
+
+    value = property(get_value, set_value)
